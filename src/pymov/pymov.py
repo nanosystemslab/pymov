897a3813d8832252992e853c4afc04c1a00b3cc9
ethnchiu <chiue@hawaii.edu>
2024-11-17 17:26:45 -1000

    Code formatting

--- src/pymov/pymov.py
+++ src/pymov/pymov.py
@@ -8,8 +8,15 @@ import serial
 
 
 class Valves:
-    """Create Valves object."""
-    
+    """Create Valves object.
+
+    Attributes:
+        port (str): The serial port to connect to (default "/dev/tty.usbmodem12201").
+        baudrate (int): The baud rate for serial communication (default 9600).
+        valves_available (Optional[List[str]]): List of available valves (default None).
+        valve_status (Optional[List[str]]): Initial status of each valve (default None).
+    """
+
     def __init__(
         self,
         port: str = "/dev/tty.usbmodem12201",
@@ -19,11 +26,11 @@ class Valves:
     ) -> None:
         """Initialize the Valves class.
 
-        Arguments:
-            port (str): The serial port to connect to.
-            baudrate (int): The baud rate for serial communication.
-            valves_available (Optional[List[str]]): List of available valves.
-            valve_status (Optional[List[str]]): Initial status of each valve.
+        Args:
+            port (str): Defines the serial port to connect to (default "/dev/tty.usbmodem12201").
+            baudrate (int): Defines the baud rate for serial communication (default 9600).
+            valves_available (Optional[List[str]]): Defines the list of available valves (default None).
+            valve_status (Optional[List[str]]): Defines the initial status of each valve (default None).
         """
         self.valves_available = valves_available or ["1", "2", "3", "4"]
         self.valve_status = valve_status or ["0", "0", "0", "0", "0"]
@@ -43,7 +50,7 @@ class Valves:
     def open_valve(self, mod_valve: str) -> None:
         """Open the specified valve(s).
 
-        Arguments:
+        Args:
             mod_valve (str): Valve(s) to open.
         """
         sleep(5)
@@ -58,7 +65,7 @@ class Valves:
     def close_valve(self, mod_valve: str) -> None:
         """Close the specified valve(s).
 
-        Arguments:
+        Args:
             mod_valve (str): Valve(s) to close.
         """
         sleep(5)
